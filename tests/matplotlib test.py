8a46c2dbc9efcb088d6be4078a539d2afece3df5
fbolsen <fbolsen@gmail.com>
2018-09-10 22:00:05 +0200

    Added readcredentials and moved files

---
+++ tests/matplotlib test.py
@@ -0,0 +1,19 @@
+import matplotlib
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
+matplotlib.use('module://kivy.garden.matplotlib.backend_kivy')
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+import matplotlib.pyplot as plt
+
+plt.plot([1, 23, 2, 4])
+plt.ylabel('some numbers')
+
+class MyApp(App):
+
+    def build(self):
+        box = BoxLayout()
+        box.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        return box
+
+MyApp().run()
